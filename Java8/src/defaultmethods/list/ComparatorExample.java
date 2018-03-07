f7161eeb54074ab1dcbc7e75a141c7f0361c031d
edimo sousa silva <edimosousasilva@edimos-MacBook-Air.local>
2018-03-07 00:12:48 -0300

    refactoring and using method reference

--- Java8/src/defaultmethods/list/ComparatorExample.java
+++ Java8/src/defaultmethods/list/ComparatorExample.java
@@ -6,28 +6,40 @@ import java.util.List;
 
 public class ComparatorExample {
 	public static void main(String[] args) {
-		Comparator<String> stringLengthComparator = new StringLengthComparator();
-		List<String> stringList = new ArrayList<>();
-		stringList.add("medium string!!!!!!!!");
-		stringList.add("big string!!!!!!!!!!!!!!!!!!");
-		stringList.add("small string!");
-		stringList.sort(stringLengthComparator);
+		List<String> stringList = getStringList();
 
+		// sortUsingComparator(stringList);
+		// sortUsingLambda(stringList);
+		sortUsingMethodReference(stringList);
 		System.out.println(stringList);
 
 	}
 
-}
+	private static void sortUsingMethodReference(List<String> stringList) {
+		// stringList.sort(Comparator.comparing(s -> s.length()));
+		stringList.sort(Comparator.comparing(String::length));
+	}
 
-class StringLengthComparator implements Comparator<String> {
+	public static void sortUsingLambda(List<String> stringList) {
+		stringList.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+	}
 
-	@Override
-	public int compare(String string1, String string2) {
-		if (string1.length() > string2.length())
-			return 1;
-		if (string1.length() < string2.length())
-			return -1;
-		return 0;
+	public static void sortUsingComparator(List<String> stringList) {
+		stringList.sort(new Comparator<String>() {
+			@Override
+			public int compare(String s1, String s2) {
+				return Integer.compare(s1.length(), s2.length());
+			}
+
+		});
+	}
+
+	private static List<String> getStringList() {
+		List<String> stringList = new ArrayList<>();
+		stringList.add("medium string!!!!!!!!");
+		stringList.add("big string!!!!!!!!!!!!!!!!!!");
+		stringList.add("small string!");
+		return stringList;
 	}
 
 }
--- Java8/src/defaultmethods/list/ConsumerExample.java
+++ Java8/src/defaultmethods/list/ConsumerExample.java
@@ -6,13 +6,39 @@ import java.util.function.Consumer;
 
 public class ConsumerExample {
 	public static void main(String[] args) {
+		List<String> stringList = getStringList();
+
+//		showUsingConsumer(stringList);
+//		showUsingLambda(stringList);
+		showMethodReference(stringList);
+
+	}
+
+	public static void showMethodReference(List<String> stringList) {
+		stringList.forEach(System.out::println);
+	}
+
+	public static void showUsingLambda(List<String> stringList) {
+		stringList.forEach(s -> System.out.println(s));
+	}
+
+	public static void showUsingConsumer(List<String> stringList) {
+		stringList.forEach(new Consumer<String>() {
+
+			@Override
+			public void accept(String s) {
+				System.out.println(s);
+			}
+
+		});
+	}
+
+	private static List<String> getStringList() {
 		List<String> stringList = new ArrayList<String>();
 		stringList.add("First String");
 		stringList.add("second STRING");
 		stringList.add("ThIrD StRiNg");
-		
-		stringList.forEach(new printUpperCaseConsumer());
-		
+		return stringList;
 	}
 }
 
